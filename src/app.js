e7e0f6a1b046cfc7d445dd59fe976658fb69302e
JuanMIsaurralde <isaurraldejuanm@gmail.com>
2025-01-10 19:37:51 +0000

    [sp-93] feat: cardGenerator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let simbols = ["♣", "♦", "♥", "♠"];
+let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
+
+function generateCard() {
+  let randomSimbolIndex = Math.floor(Math.random() * simbols.length);
+  let randomNumberIndex = Math.floor(Math.random() * numbers.length);
+  document.querySelector("#cardSimbolTop").innerHTML =
+    simbols[randomSimbolIndex];
+  document.querySelector("#cardSimbolBottom").innerHTML =
+    simbols[randomSimbolIndex];
+  document.querySelector("#cardNumber").innerHTML = numbers[randomNumberIndex];
+  let simbolColor =
+    simbols[randomSimbolIndex] == "♦" || simbols[randomSimbolIndex] == "♥"
+      ? "text-danger"
+      : "text-dark";
+  document.querySelector("#cardSimbolTop").className = simbolColor;
+  document.querySelector("#cardSimbolBottom").className = simbolColor;
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateCard();
+  document
+    .querySelector("#shuffleCard")
+    .addEventListener("click", generateCard);
 };
